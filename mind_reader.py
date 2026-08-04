79b9c90ad94b4c138339755477e0d204dd0505b4
Jal Mavani <jalmavani027@gmail.com>
2026-08-04 23:08:16 +0530

    Add AI Mind Reader 3000 functionality

---
+++ mind_reader.py
@@ -0,0 +1,34 @@
+import time
+
+print("=" * 50)
+print("        AI MIND READER 3000")
+print("=" * 50)
+
+input("\nThink of any number (don't tell me). Press Enter...")
+
+print("\nAnalyzing brain waves...")
+for i in range(3):
+    print("." * (i + 1))
+    time.sleep(1)
+
+input("\nMultiply your number by 2. Press Enter...")
+input("Now add 10. Press Enter...")
+input("Now divide by 2. Press Enter...")
+input("Now subtract your original number. Press Enter...")
+
+print("\nScanning thoughts...")
+time.sleep(2)
+
+print("\n🧠 I know your answer...")
+
+time.sleep(2)
+
+print("""
+██████████████████████
+█                  █
+█        5         █
+█                  █
+██████████████████████
+""")
+
+print("😎 Was I right?")
